e63e4ec95732b499cd659c19c06995be19ad3183
JVCM123 <jvcmoura2012@gmail.com>
2025-11-03 23:58:29 -0300

    alteração campo

--- SistDist.Domain/Models/Usuario.cs
+++ SistDist.Domain/Models/Usuario.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Nome { get; set; }
-        public string Senha { get; set; }
+        public string Telefone { get; set; }
         public string Email { get; set; }
         public DateTime DtNascimento { get; set; }
         public string Cidade { get; set; }
